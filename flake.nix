c459b8909ced6ec3250174485b94ca4f97063b92
Gabriel <gabrielhenrik25@gmail.com>
2026-06-24 09:14:21 -0300

    Vars

--- flake.nix
+++ flake.nix
@@ -1,24 +1,33 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations.nix = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.noite = import ./home.nix;
-        }
-      ];
-    };
-  };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+	outputs = { self, nixpkgs, home-manager, ... }:
+	let
+	vars = {
+		hostName = "nix";
+		user     = "noite";
+		timezone = "America/Sao_Paulo";
+	};
+	in
+	{
+		nixosConfigurations.${vars.hostName} = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			specialArgs = { inherit vars; };
+			modules = [
+				./configuration.nix
+				./hardware-configuration.nix
+				home-manager.nixosModules.home-manager
+				{
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.${vars.user} = import ./home.nix;
+				}
+			];
+		};
+	};
 }

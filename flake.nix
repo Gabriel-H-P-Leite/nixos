129640c84b285a6e3b4d8749e9bf853613ea6341
Gabriel <gabrielhenrik25@gmail.com>
2026-06-24 09:19:24 -0300

    Vars

--- flake.nix
+++ flake.nix
@@ -21,10 +21,10 @@
 			modules = [
 				./configuration.nix
 				./hardware-configuration.nix
-				home-manager.nixosModules.home-manager
-				{
+				home-manager.nixosModules.home-manager {
 					home-manager.useGlobalPkgs = true;
 					home-manager.useUserPackages = true;
+					home-manager.extraSpecialArgs = { inherit vars; };
 					home-manager.users.${vars.user} = import ./home.nix;
 				}
 			];

6f5a7b55294e3da196f472fccf030bf1fcc5a480
Gabriel <gabrielhenrik25@gmail.com>
2026-09-22 13:07:37 -0300

    xwayland

--- packages.nix
+++ packages.nix
@@ -1,20 +1,22 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
 	#PACOTES
 	nixpkgs.config.allowUnfree = true;
 	environment.systemPackages = with pkgs; [
 		#INTERFACE 
-		niri quickshell nwg-look 
+		niri quickshell nwg-look pkgs.xwayland-satellite 
 		#AUDIO
 		pipewire wireplumber
 		#APPS
 		firefox kitty pwvucontrol blueman thunar thunar-volman thunar-media-tags-plugin thunar-shares-plugin ffmpegthumbnailer tumbler gvfs gparted 
 		#MIDIA
-		feh mpd mpd-mpris rmpc mpv playerctl
+		mpd mpd-mpris rmpc mpv playerctl
 		#CLI
 		fastfetch btop less libnotify yt-dlp ffmpeg unzip github-cli flatpak git wget wev
 		#TEXTO
-		neovim mousepad zathura zathuraPkgs.zathura_pdf_mupdf
+		neovim mousepad zathura zathuraPkgs.zathura_pdf_mupdf 
+		#IMAGENS
+		imv libtiff libpng libjpeg lcms2 librsvg libheif libjxl libwebp
 	];
 	fonts.packages = with pkgs; [
 		noto-fonts
@@ -34,7 +36,7 @@
 		enable = true;
 		shellAliases = {
 			nrs = "sudo nixos-rebuild switch --flake /etc/nixos";
-			nrsu = "sudo nixos-rebuild switch --flake --upgrade";
+			nrsu = "sudo nixos-rebuild switch --flake /etc/nixos --upgrade";
 			ncon = "sudo nvim /etc/nixos/configuration.nix";
 			ndel = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +5";
 			hms = "home-manager switch";
@@ -44,5 +46,12 @@
 	};
 	programs.git = {
 		enable = true;
+	};
+	xdg.portal = {
+		enable = true;
+		extraPortals = [
+			pkgs.xdg-desktop-portal-gtk
+			pkgs.xdg-desktop-portal-wlr
+		];
 	};
 }
